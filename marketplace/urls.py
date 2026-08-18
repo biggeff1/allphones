f1bf81cfeca87ff3ab3caf1796a68effc17e3e46
biggeff1 <ntambwej505@gmail.com>
2026-08-19 00:39:44 +0200

    feat: add seller deposit route

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from . import views
-
 urlpatterns = [
     path("", views.home, name="home"),
+    path("deposer/", views.deposit, name="deposit"),
     path("annonce/<int:pk>/", views.listing_detail, name="listing_detail"),
     path("annonce/<int:pk>/interesse/", views.interest, name="interest"),
 ]
